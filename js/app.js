e6a2549b1162f58e80538a54637732f2ee25edec
Olivier Lartillot <olivier.lartillot@gmail.com>
2022-04-22 11:21:52 +0200

    Separation of the dragon board in a module

--- js/app.js
+++ js/app.js
@@ -2,15 +2,7 @@
 // Notre variable qui contient le "module" app (un objet)
 let app = {
     // Tableau contenant des objets dragons
-    dragons: [
-        {nom: 'Grand Maitre', img: 'grand-maitre.jpg', desc: 'Si tu te prends encore pour moi, ce n\'est pas en dragon que je vais te changer' },
-        {nom: 'Gon', img: 'enfant.jpg', desc: 'Tu es et tu resteras un enfant, joueur et espiègle' },
-        {nom: 'Fier', img: 'fier.jpg', desc: 'Tu es un dragon fier, pas prêt à te faire écailler'  },
-        {nom: 'Luminoso', img: 'lumineux.jpg', desc: 'Quelles écailles brilantes tu as là ... et tu aimes les montrer ! '  },
-        {nom: 'Coloré', img: 'colored.jpg', desc: 'Tu es un dragon haut en couleur, un peu foufou, mais on t\'aime bien quand même' },
-        {nom: 'D\'OR', img: 'dragon-d-or.jpg', desc: 'Tu es sans nul doute le plus beau et le plus rare des Dragons' },
-
-    ],
+    dragons,
     // Méthode appelée au chargement de la page
     init: function() {
 
